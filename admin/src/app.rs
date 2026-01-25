6e8295366ce11403c585dfca111f7d4dfce4ce5c
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-01-25 19:30:33 +0100

    feature(admin): move header into proper component

--- admin/src/app.rs
+++ admin/src/app.rs
@@ -1,102 +1,64 @@
 use std::sync::Arc;
 
 use crate::{
-    service::{AdminService, Contest},
+    service::AdminService,
     ui::body::{self, Body},
-    ui::header::{self, Tab},
+    ui::header::{self, Header},
 };
 use anyhow::Result;
-use chrono::Utc;
-use iced::{Element, Task, Theme, time, widget::column};
+use iced::{Element, Task, Theme, widget::column};
 
 #[derive(Debug)]
 struct AdminApp {
-    active_tab: Tab,
     service: Arc<dyn AdminService>,
-    contest: Option<Contest>,
-    time_remaining: Option<chrono::Duration>,
     body: Body,
+    header: Header,
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
-    TabChanged(Tab),
-    LoadContest,
-    ContestLoaded(Result<Option<Contest>, String>),
     Body(body::Message),
-    Tick,
+    Header(header::Message),
 }
 
 impl AdminApp {
     fn new(service: Arc<dyn AdminService>) -> (Self, Task<Message>) {
         let (body, body_task) = Body::new();
+        let (header, header_task) = Header::new();
         let state = Self {
             service: service.clone(),
-            active_tab: Tab::Stations,
-            contest: None,
-            time_remaining: None,
             body,
+            header,
         };
 
         // load inital data
         let task = Task::batch(vec![
-            Task::done(Message::LoadContest),
             body_task.map(Message::Body),
+            header_task.map(Message::Header),
         ]);
 
         (state, task)
     }
 
     fn view(&self) -> Element<'_, Message> {
-        let header = header::view(self.contest.clone(), self.time_remaining);
-        let body = self.body.view(self.active_tab).map(Message::Body);
+        let header = self.header.view().map(Message::Header);
+        let body = self
+            .body
+            .view(self.header.get_active_tab())
+            .map(Message::Body);
         column![header, body].into()
     }
 
     fn update(&mut self, message: Message) -> Task<Message> {
         let service = self.service.clone();
         match message {
-            Message::TabChanged(tab) => {
-                self.active_tab = tab;
-                Task::none()
-            }
-            Message::LoadContest => Task::perform(
-                async move { service.fetch_contest().await.map_err(|e| e.to_string()) },
-                Message::ContestLoaded,
-            ),
-            Message::ContestLoaded(contest) => match contest {
-                Ok(result) => {
-                    self.contest = result.clone();
-                    if let Some(contest) = result {
-                        let now = Utc::now();
-                        self.time_remaining = Some(contest.start_time.signed_duration_since(now))
-                    }
-                    Task::none()
-                }
-                Err(msg) => {
-                    println!("failed to load contest: {msg}");
-                    Task::none()
-                }
-            },
-            Message::Tick => {
-                if let Some(contest) = &mut self.contest {
-                    let now = Utc::now();
-                    self.time_remaining = Some(contest.start_time.signed_duration_since(now));
-                    if contest.end_time.lt(&now) {
-                        // if contest is over, fetch the next one
-                        return Task::done(Message::LoadContest);
-                    }
-                } else {
-                    self.time_remaining = None
-                }
-                Task::none()
-            }
             Message::Body(message) => self.body.update(message, service).map(Message::Body),
+            Message::Header(message) => self.header.udpate(message, service).map(Message::Header),
         }
     }
 
     fn subscription(&self) -> iced::Subscription<Message> {
-        time::every(std::time::Duration::from_secs(1)).map(|_| Message::Tick)
+        self.header.subscription().map(Message::Header)
     }
 
     fn style(&self) -> Theme {
--- admin/src/ui/header.rs
+++ admin/src/ui/header.rs
@@ -1,9 +1,12 @@
-use crate::app::Message;
-use crate::service::Contest;
-use chrono::Duration;
-use iced::widget::space::horizontal;
-use iced::widget::{button, container, row, text};
-use iced::{Background, Color, Element, Font, Length, Shadow, Vector, alignment};
+use std::sync::Arc;
+
+use chrono::{Duration, Utc};
+use iced::{
+    Background, Color, Element, Font, Length, Shadow, Task, Vector, alignment, time,
+    widget::{button, container, row, space::horizontal, text},
+};
+
+use crate::service::{AdminService, Contest};
 
 #[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Tab {
@@ -12,44 +15,98 @@ pub enum Tab {
     Teams,
 }
 
-pub fn view(
+#[derive(Debug, Default)]
+pub struct Header {
+    active_tab: Tab,
     contest: Option<Contest>,
-    time_remaining: Option<Duration>,
-) -> Element<'static, Message> {
-    let mut nav_bar = row![
-        tab_button("Stations", Tab::Stations),
-        tab_button("Teams", Tab::Teams),
-        horizontal(),
-    ]
-    .spacing(20);
-    if let Some(contest) = contest
-        && let Some(time_remaining) = time_remaining
-    {
-        nav_bar = nav_bar.push(contest_info(contest.name.clone(), time_remaining));
-    } else {
-        nav_bar = nav_bar.push(text("No contest found").font(Font::MONOSPACE).size(32))
-    }
-
-    container(nav_bar)
-        .width(Length::Fill)
-        .height(Length::Shrink)
-        .padding(10)
-        .style(header_style)
-        .into()
+    time_remaining: Option<chrono::Duration>,
 }
 
-fn tab_button(label: &str, tab: Tab) -> Element<'static, Message> {
-    button(
-        text(label.to_string())
-            .align_x(alignment::Horizontal::Center)
-            .align_y(alignment::Vertical::Center)
-            .height(Length::Fill),
-    )
-    .on_press(Message::TabChanged(tab))
-    .padding([5, 5])
-    .height(Length::Fill)
-    .width(150)
-    .into()
+#[derive(Debug, Clone)]
+pub enum Message {
+    LoadContest,
+    ContestLoaded(Result<Option<Contest>, String>),
+    Tick,
+    TabChanged(Tab),
+}
+
+impl Header {
+    pub fn new() -> (Self, Task<Message>) {
+        (Self::default(), Task::done(Message::LoadContest))
+    }
+
+    pub fn view(&self) -> Element<'_, Message> {
+        let mut nav_bar = row![
+            tab_button("Stations", Tab::Stations),
+            tab_button("Teams", Tab::Teams),
+            horizontal(),
+        ]
+        .spacing(20);
+        if let Some(contest) = self.contest.clone()
+            && let Some(time_remaining) = self.time_remaining
+        {
+            nav_bar = nav_bar.push(contest_info(contest.name.clone(), time_remaining));
+        } else {
+            nav_bar = nav_bar.push(text("No contest found").font(Font::MONOSPACE).size(32))
+        }
+
+        container(nav_bar)
+            .width(Length::Fill)
+            .height(Length::Shrink)
+            .padding(10)
+            .style(header_style)
+            .into()
+    }
+
+    pub fn udpate(&mut self, message: Message, service: Arc<dyn AdminService>) -> Task<Message> {
+        match message {
+            Message::LoadContest => Task::perform(
+                async move { service.fetch_contest().await.map_err(|e| e.to_string()) },
+                Message::ContestLoaded,
+            ),
+            Message::ContestLoaded(contest) => match contest {
+                Ok(result) => {
+                    self.contest = result.clone();
+                    if let Some(contest) = result {
+                        let now = Utc::now();
+                        self.time_remaining = Some(contest.start_time.signed_duration_since(now))
+                    }
+                    Task::none()
+                }
+                Err(msg) => {
+                    println!("failed to load contest: {msg}");
+                    Task::none()
+                }
+            },
+            Message::Tick => self.handle_tick(),
+            Message::TabChanged(tab) => {
+                self.active_tab = tab;
+                Task::none()
+            }
+        }
+    }
+
+    fn handle_tick(&mut self) -> Task<Message> {
+        if let Some(contest) = &mut self.contest {
+            let now = Utc::now();
+            self.time_remaining = Some(contest.start_time.signed_duration_since(now));
+            if contest.end_time.lt(&now) {
+                // if contest is over, fetch the next one
+                return Task::done(Message::LoadContest);
+            }
+        } else {
+            self.time_remaining = None
+        }
+        Task::none()
+    }
+
+    pub fn subscription(&self) -> iced::Subscription<Message> {
+        time::every(std::time::Duration::from_secs(1)).map(|_| Message::Tick)
+    }
+
+    pub fn get_active_tab(&self) -> Tab {
+        self.active_tab
+    }
 }
 
 fn header_style(theme: &iced::Theme) -> container::Style {
@@ -69,6 +126,20 @@ fn header_style(theme: &iced::Theme) -> container::Style {
         },
         ..container::Style::default()
     }
+}
+
+fn tab_button(label: &str, tab: Tab) -> Element<'static, Message> {
+    button(
+        text(label.to_string())
+            .align_x(alignment::Horizontal::Center)
+            .align_y(alignment::Vertical::Center)
+            .height(Length::Fill),
+    )
+    .on_press(Message::TabChanged(tab))
+    .padding([5, 5])
+    .height(Length::Fill)
+    .width(150)
+    .into()
 }
 
 fn contest_info(contest_name: String, time_remaining: Duration) -> Element<'static, Message> {
