880351243a9ae10c601edeed7cab29c5e6b755a5
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-03-15 22:28:24 +0100

    rewrite wallpaper handler

--- backend-v2/src/api.rs
+++ backend-v2/src/api.rs
@@ -2,12 +2,51 @@ pub mod admin;
 pub mod broadcast;
 pub mod map;
 pub mod station;
+pub mod wallpaper;
 
+use axum::http::StatusCode;
 use chrono::{DateTime, Utc};
 use prost_types::Timestamp;
 use tonic::Status;
 
 use crate::error::AppError;
+
+use axum::{
+    Json,
+    response::{IntoResponse, Response},
+};
+use serde_json::json;
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self {
+            AppError::NotFound(msg) => (StatusCode::NOT_FOUND, msg),
+            AppError::InvalidArgument(msg) => (StatusCode::BAD_REQUEST, msg),
+            AppError::FailedPrecondition(msg) => (StatusCode::PRECONDITION_FAILED, msg),
+            AppError::Internal(msg) => {
+                tracing::error!("Internal server error: {}", msg);
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "An internal server error occurred".to_string(),
+                )
+            }
+            AppError::AlreadyExists(msg) => (StatusCode::BAD_REQUEST, msg),
+            AppError::Database(msg) => {
+                tracing::error!("Database error: {}", msg);
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "An internal server error occurred".to_string(),
+                )
+            }
+        };
+
+        let body = Json(json!({
+            "error": error_message,
+        }));
+
+        (status, body).into_response()
+    }
+}
 
 impl From<AppError> for Status {
     fn from(err: AppError) -> Self {
@@ -21,6 +60,19 @@ impl From<AppError> for Status {
                 tracing::error!(error = %e, "database error");
                 Status::internal("internal database error")
             }
+        }
+    }
+}
+
+impl From<AppError> for StatusCode {
+    fn from(value: AppError) -> Self {
+        match &value {
+            AppError::NotFound(_) => StatusCode::NOT_FOUND,
+            AppError::InvalidArgument(_) => StatusCode::BAD_REQUEST,
+            AppError::AlreadyExists(_) => StatusCode::NOT_MODIFIED,
+            AppError::FailedPrecondition(_) => StatusCode::PRECONDITION_FAILED,
+            AppError::Internal(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            AppError::Database(_) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 }
---
+++ backend-v2/src/api/wallpaper.rs
@@ -0,0 +1,74 @@
+use std::sync::Arc;
+
+use axum::{
+    body::Body,
+    extract::{Query, State},
+    http::Response,
+    response::IntoResponse,
+};
+use derive_more::derive::Constructor;
+use reqwest::header;
+use serde::Deserialize;
+use tokio_stream::StreamExt as _;
+
+use crate::{
+    domain::{ContestRepository, TeamRepository},
+    error::AppError,
+};
+
+#[derive(Constructor)]
+pub struct WallpaperHandler {
+    constest_repo: Arc<dyn ContestRepository>,
+    team_repo: Arc<dyn TeamRepository>,
+}
+
+#[derive(Deserialize)]
+pub struct WallpaperParams {
+    pub ip: Option<String>,
+    pub contest_id: Option<String>,
+}
+
+pub async fn wallpaper_handler(
+    State(state): State<WallpaperHandler>,
+    Query(query): Query<WallpaperParams>,
+) -> Result<impl IntoResponse, AppError> {
+    let contest_id = if let Some(id) = query.contest_id {
+        id
+    } else {
+        let contest_option = state.constest_repo.get_next_contest().await?;
+        contest_option
+            .map(|c| c.id)
+            .ok_or_else(|| AppError::NotFound("no upcomming contest found".to_string()))?
+    };
+    // get the wallpaper
+    let wallpaper = state
+        .constest_repo
+        .get_wallpaper(&contest_id)
+        .await?
+        .ok_or_else(|| AppError::NotFound("no wallpaper found".to_string()))?;
+    // create stream from database stream
+    let body_stream = wallpaper
+        .stream
+        .map(|res| res.map(bytes::Bytes::from).map_err(std::io::Error::other));
+
+    // try to get team name
+    let team_name = if let Some(ip) = query.ip {
+        let team = state.team_repo.get_by_ip(&ip).await?;
+        Some(team.name)
+    } else {
+        None
+    };
+
+    // create request builder
+    let mut builder = Response::builder().header(header::CONTENT_TYPE, wallpaper.mime_type);
+
+    // if teamname, set headers
+    if let Some(team_name) = team_name {
+        builder = builder.header("X-Wallpaper-Text", team_name);
+        builder = builder.header("X-Wallpaper-Text-Color", wallpaper.text_color);
+    }
+
+    builder
+        .body(Body::from_stream(body_stream))
+        .map_err(|e| AppError::Internal(e.to_string()))
+}
